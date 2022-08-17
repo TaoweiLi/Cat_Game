19179b1f8090468814c602d7801b00ed6d002882
TaoweiLi <104708757+TaoweiLi@users.noreply.github.com>
2022-08-16 17:59:04 -0700

    added tree png

--- src/obstacle.js
+++ src/obstacle.js
@@ -7,23 +7,67 @@ class Obstacle extends Component {
 
   static assets = [
     {
-      'urlPath': "https://raw.githubusercontent.com/TaoweiLi/Cat_Game_Final_Proposal/main/asset/obstacle1.png",
-      'width' : 50,  //square size
-      'height': 90,
-      'xOffset': 70, 
-      'yOffset': 50,
-      'displayRatio' : 1.8 //obstacles size
+      'urlPath': "https://raw.githubusercontent.com/TaoweiLi/Cat_Game_Final_Proposal/main/asset/cherry.png",
+      'width' : 70,  //square size
+      'height': 100,
+      'xOffset': 45, 
+      'yOffset': 70,
+      'displayRatio' : 1.5 //obstacles size
     }
-,
+    ,
     {
       'urlPath': "https://raw.githubusercontent.com/TaoweiLi/Cat_Game_Final_Proposal/main/asset/avocade.png",
-      'width': 100,
-      'height': 70,
-      'xOffset': 70,
-      'yOffset': 50 ,
-      'displayRatio': 1.0
+      'width': 70,
+      'height': 100,
+      'xOffset': 55,
+      'yOffset': 70,
+      'displayRatio': 1.5
     }
-  ]
+    ,
+    {
+      'urlPath': "https://raw.githubusercontent.com/TaoweiLi/Cat_Game_Final_Proposal/main/asset/mango.png",
+      'width': 100,
+      'height': 80,
+      'xOffset': 30,
+      'yOffset': 34,
+      'displayRatio': 1.5
+    }
+    ,
+    {
+      'urlPath': "https://raw.githubusercontent.com/TaoweiLi/Cat_Game_Final_Proposal/main/asset/orange.png",
+      'width': 100,
+      'height': 80,
+      'xOffset': 25,
+      'yOffset': 70,
+      'displayRatio': 1.4
+    }
+    ,
+    {
+      'urlPath': "https://raw.githubusercontent.com/TaoweiLi/Cat_Game_Final_Proposal/main/asset/watermelon.png",
+      'width': 100,
+      'height': 90,
+      'xOffset': 45,
+      'yOffset': 33,
+      'displayRatio': 1.4
+    }
+    ,
+    {
+      'urlPath': "https://raw.githubusercontent.com/TaoweiLi/Cat_Game_Final_Proposal/main/asset/cherry.png",
+      'width': 70,  //square size
+      'height': 100,
+      'xOffset': 45,
+      'yOffset': 70,
+      'displayRatio': 1.5 //obstacles size
+    }
+    , {
+      'urlPath': "https://raw.githubusercontent.com/TaoweiLi/Cat_Game_Final_Proposal/main/asset/cherry.png",
+      'width': 70,  //square size
+      'height': 100,
+      'xOffset': 45,
+      'yOffset': 70,
+      'displayRatio': 1.5 //obstacles size
+    }
+  ] 
 
   constructor(x, canvasHeight) {
     let random = Math.floor(Math.random() * Obstacle.assets.length);
